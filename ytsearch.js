7df95763168d675d1883d4c28f9542cb84f4a181
lumi192 <robertsoncruz192@gmail.com>
2025-10-07 20:34:43 +0800

    uses yt-search instead of yt-dlp, much faster

---
+++ ytsearch.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import yts from "yt-search";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/search", async (req, res) => {
+    const rawSearchResults = await yts(req.body.query);
+    const searchResults = rawSearchResults.all.map((video) => ({title: video.title, url: video.url}));
+    res.json(searchResults);
+});
+
+/*
+app.post("/watch", async (req, res) => {
+    console.log(req.body.url);
+
+    res.json({src});
+}); 
+*/
+
+app.listen(1922, () => {
+    console.log("online at http://localhost:1922/");
+}); 
+
+
